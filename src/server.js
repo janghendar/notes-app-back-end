faffb1751d8dadc71fce27bf5a75a9e103fcf95f
Suhendar Aryadi <suhendarweb@gmail.com>
2021-04-19 22:41:48 +0700

    Update server.js
    Mengganti alamat dns AWS

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : 'ec2-13-229-128-200.ap-southeast-1.compute.amazonaws.com',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : 'ec2-3-0-94-249.ap-southeast-1.compute.amazonaws.com',
     routes: {
       cors: {
         origin: ['*'],
